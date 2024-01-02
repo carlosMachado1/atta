40e7d0a5cbb6585974072d9955bc03d51385c30a
Carlos Gabriel da Silva Machado <carlos.machado1980@yahoo.com>
2024-01-02 04:56:34 -0400

    feat: Add cors and dotenv
    List of changes:
    - Added cors and dontenv config
    - Created a simple route, just to test
    - Added space between braces and brackets for eslint

--- desenvolvimento/atta-backend/src/app.ts
+++ desenvolvimento/atta-backend/src/app.ts
@@ -1,1 +1,17 @@
-console.log(1234);
+import "dotenv/config";
+import cors from "cors";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json);
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+const port = 3000; 
+
+app.listen(port, () => {
+    console.log("Servidor rodando na porta " + port);
+});
